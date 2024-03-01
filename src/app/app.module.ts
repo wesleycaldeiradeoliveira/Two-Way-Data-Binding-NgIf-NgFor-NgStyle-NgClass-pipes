8747b5a9eb7e73e4ff4b6d96d14fe2d7830fe7ec
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-03-01 10:28:05 -0300

    Diretiva NgFor com Componentes Filhos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
