f0a8ee3eeee511814c891e2837da886ae4382dfe
wesleycaldeiradeoliveira <wesleycaldeiradeoliveira>
2024-03-02 09:14:14 -0300

    Diretiva de Atributo NgClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,40 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  fontSize: number = 30;
-  textColor: 'white' | 'orange' = 'white';
-  buttonText: 'Branco' | 'Laranja' = 'Laranja';
-  stylesString: string = 'font-size: 30px; color: white';
-  stylesObj = {
-    'font-size': this.fontSize + 'px',
-    'color': this.textColor,
-  };
+  isGreen: boolean = true;
 
-  increaseFontSize() {
-    this.fontSize += 5;
-
-    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
-
-    this.stylesObj = {
-      'font-size': this.fontSize + 'px',
-      'color': this.textColor,
-    };
+  setToGreen() {
+    this.isGreen = true;
   }
 
-  toggleFontColor() {
-    if(this.textColor === 'white') {
-      this.textColor = 'orange';
-      this.buttonText = 'Branco';
-    } else {
-      this.textColor = 'white';
-      this.buttonText = 'Laranja';
-    }
-
-    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
-
-    this.stylesObj = {
-      'font-size': this.fontSize + 'px',
-      'color': this.textColor,
-    };
+  setToOrange() {
+    this.isGreen = false;
   }
 }
